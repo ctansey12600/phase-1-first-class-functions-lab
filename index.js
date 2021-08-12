3609535784fbe0cae1d6e01fc133b868fecad0aa
caseytansey <ctansey12600@outlook.com>
2021-08-12 19:50:31 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const returnLastTwoDrivers = array => array.slice((array.length - 2),(array.leng
 let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
 
 function createFareMultiplier(integer){
-    return function multiply(){
-        return integer * integer
+    return function(fare){
+        return fare * integer
     }
 }
 
-const fareDoubler = fare => fare * 2
+const fareDoubler = createFareMultiplier(2)
 
-const fareTripler = fare => fare * 3
+const fareTripler = createFareMultiplier(3)
 
 function selectDifferentDrivers(array, f){
     return f(array)
